82ff8db78398064d1d8d49f3d6e2e2ab0f216e70
sunflowerseat <sunflowerseat@gmail.com>
2023-04-17 15:18:53 +0800

    init

---
+++ docs/.vuepress/config/nav.ts
@@ -0,0 +1,38 @@
+// nav 配置, 即上方导航栏
+
+import { NavItem } from "vuepress/config";
+
+export default <Array<NavItem>>[
+  { text: "Home", link: "/" },
+  {
+    text: "Space",
+    link: "/space/",
+    items: [
+      {
+        text: "Quick Start",
+        link: "/pages/33ba0f/",
+      },
+      {
+        text: "Adding Users to your Project",
+        link: "/pages/492541/",
+      },
+    ],
+  },
+  {
+    text: "Firedoc - Guide",
+    link: "/pages/6b925b/",
+  },
+  {
+    text: "Link",
+    items: [
+      {
+        text: "Ctrldoc Website",
+        link: "https://k8suat.ctrldoc.com/web_ux/",
+      },
+      {
+        text: "Contact Us",
+        link: "https://k8suat.ctrldoc.com/",
+      },
+    ],
+  },
+];
